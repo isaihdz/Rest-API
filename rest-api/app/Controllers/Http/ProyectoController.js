ea2058d8e29d86fba15bf7d8c6466520d36db4ca
Iasi <>
2020-02-10 13:41:07 -0600

    Método Update

--- rest-api/app/Controllers/Http/ProyectoController.js
+++ rest-api/app/Controllers/Http/ProyectoController.js
@@ -27,6 +27,16 @@ class ProyectoController {
         await proyecto.delete();
         return proyecto;
     }
+
+    async update ({auth , request, params}){
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto  = await  Proyecto.find(id);
+        AutorizacionService.verificarPermiso(proyecto,user);
+        proyecto.merge(request.only('nombre'));
+        await proyecto.save();
+        return proyecto;
+    }
 }
 
 module.exports = ProyectoController
